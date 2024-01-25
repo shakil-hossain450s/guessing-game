23e2d4da1a60763d01e30b8d7d8e6f67335637cd
unknown <shakil.hossain450s@gmail.com>
2024-01-25 21:15:52 +0600

    some changes

--- js/index.js
+++ js/index.js
@@ -2,7 +2,9 @@ let numOfWon = 0;
 let numOfLost = 0;
 
 for (let i = 1; i <= 5; i++) {
-  let guessNumber = parseInt(prompt("Enter a number from 1 to 5: "));
+  let guessNumber = parseInt(
+    prompt("First Open your console. Then Enter a number from 1 to 5: ")
+  );
   let randomNumber = Math.floor(Math.random() * 5) + 1;
 
   guessNumber == randomNumber
