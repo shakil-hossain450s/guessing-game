543b5249872520f66cca9079e0dc0ef5d2a3319a
unknown <shakil.hossain450s@gmail.com>
2024-01-25 21:12:56 +0600

    guessing game

---
+++ js/index.js
@@ -0,0 +1,15 @@
+let numOfWon = 0;
+let numOfLost = 0;
+
+for (let i = 1; i <= 5; i++) {
+  let guessNumber = parseInt(prompt("Enter a number from 1 to 5: "));
+  let randomNumber = Math.floor(Math.random() * 5) + 1;
+
+  guessNumber == randomNumber
+    ? (console.log("You have won..."), numOfWon++)
+    : (console.log("You have lost. Random number was " + randomNumber),
+      numOfLost++);
+}
+
+document.write("Total Number of won: " + numOfWon + "</br>");
+document.write("Total Number of lost: " + numOfLost);
